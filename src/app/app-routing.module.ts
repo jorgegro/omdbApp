0cc754569fb0c23f38ed6bc84b637f0f62861416
Josue Plaza <plazajosue2@gmail.com>
2018-07-11 15:54:20 -0500

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
 
-// STEP 3 
 const routes: Routes = [
-  // Routes go here
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "question/:id",
+    component: QuestionComponent
+  },
+  {
+    path: "result",
+    component: ResultComponent
+  },
+  {
+    // DEFAULT PATHING
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  }
 ];
-// STEP 4 
 
-// STEP 5
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
